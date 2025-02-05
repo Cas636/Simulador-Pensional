1fd40f0ab81e15ca737cde622441bc188d7aab26
Jhony Caro <jcaro@truora.com>
2025-02-05 13:43:32 -0500

    Adision de mensaje cuando no hay opciones de pension

--- front-end/src/components/SimulationResult.js
+++ front-end/src/components/SimulationResult.js
@@ -1,24 +1,30 @@
 import React from "react";
-import '../assets/SimulationResult.css';  // Asegúrate de importar el archivo CSS
+import '../assets/SimulationResult.css'; 
 
 const SimulationResult = ({ userName, simulationData }) => {
   if (!simulationData) return <p>Cargando resultados...</p>;
 
   return (
     <div className="simulation-result">
-      <h2 className="result-title">Resultados de la Simulación para <span className="user-name">{userName}</span></h2>
+      <h2 className="result-title">
+        Resultados de la Simulación para <span className="user-name">{userName}</span>
+      </h2>
       {simulationData.map((regimenData, index) => (
         <div key={index} className="regimen-section">
           <h3 className="regimen-title">Régimen: {regimenData.regimen}</h3>
-          <ul className="options-list">
-            {regimenData.opciones.map((opcion, idx) => (
-              <li key={idx} className="option-item">
-                <strong className="option-priority">Prioridad {opcion.prioridad}:</strong> 
-                <span className="option-weeks">{opcion.semanas} semanas</span> - 
-                <span className="option-amount">${opcion.monto.toFixed(2)}</span>
-              </li>
-            ))}
-          </ul>
+          {regimenData.opciones.length === 0 ? (
+            <p className="no-pension-message">No se cumplen los requisitos para obtener una pensión 🙁</p>
+          ) : (
+            <ul className="options-list">
+              {regimenData.opciones.map((opcion, idx) => (
+                <li key={idx} className="option-item">
+                  <strong className="option-priority">Prioridad {opcion.prioridad}:</strong> 
+                  <span className="option-weeks">{opcion.semanas} semanas</span> - 
+                  <span className="option-amount">${opcion.monto.toFixed(2)}</span>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       ))}
     </div>
